d3c0b33215f0aee622c76e9626237ebffc439f7e
ananyanath2017 <ananyanath2017@gmail.com>
2024-06-30 23:52:40 +0530

    updated code

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Style.css';
 import Logo from './chef-hat.png';
+import Vector from './Vector.png';
+import Line from './Line_1.png';
+import Shopping_cart from './Shopping_cart.png';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -10,7 +13,8 @@ function Header() {
     <div>
       <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home"> <img src={Logo} className="Logo"></img></Navbar.Brand>
+                <Navbar.Brand href="#home"> 
+                  <img src={Logo} className="Logo"></img><h1 className="foo">Foo</h1></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -18,6 +22,10 @@ function Header() {
                         <Nav.Link className="menu" href="#menu">Menu</Nav.Link>
                         <Nav.Link className="menu" href="#contact">Contact</Nav.Link>
                         <Nav.Link className="menu" href="#shop">Shop</Nav.Link>
+                        <img src={Vector} className='vector'></img>
+                        <Nav.Link className="menu1" href="#search">Search</Nav.Link>
+                        <img src={Line} className="Line_1"></img>
+                        <img src={Shopping_cart} className="Shopping_cart"></img>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,32 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import './Style.css';
 import Fastfood from './Fast_food.png';
+import Group_4 from './Group_4.png';
+import Group_3 from './Group_3.png';
+import Group_2 from './Group_2.png';
+
 
 function Home() {
+  const [an, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <Container>
       <Row>
         <Col>
         <Container>
           <Row>
-
+          <h1 className="fast">Fast</h1>
           </Row>
           <Row>
-
+          <h3 className="food_delivery">Food Delivery</h3>
           </Row>
           <Row>
-
+          <img src={Group_4} className="Group_4"></img>
           </Row>
           <Row>
-
+          <Button variant="primary" className='button' onClick={handleShow}>
+              Order Now
+            </Button>
+            <Container>
+              <Row>
+              <h3 className="Watch_Vedio">Watch Vedio</h3>
+              </Row>
+              <Row>
+              <img src={Group_2} className="Group_2"></img>
+              </Row>
+              <Row>
+              <img src={Group_3} className="Group_3"></img>
+              </Row>
+            </Container>
           </Row>
         </Container>
         </Col>
       <Col>
-        <img src={Fastfood} className='fast food'></img>
+        <img src={Fastfood} className='fast_food'></img>
       </Col>
       </Row>
     </Container >
